7cb97c603329ee3b36285e6f70dc636310685996
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-09-06 20:33:35 +0530

    module

--- hello.js
+++ hello.js
@@ -1,8 +1,27 @@
-console.log('hey there');
+const math = require('./math')
+
+// const math = require("fs")
+// const { add , sub } = require('./math')  // destructuring  means jo bhi variable math vali module se aaye
 
 
-// console.log(window);
+// console.log( "math value is : " + math.addfn(2,4));
+// console.log( "math value is : " + math.subfn(2,4));
 
-// console.alert('hello')
+// console.log( "math value is : " + math.add(2,4));
+// console.log( "math value is : " + math.sub(2,4));
+
+
+// console.log( "math value is : " + add(2,4));
+// console.log( "math value is : " + sub(2,4));
+
+
+// console.log("inside math module value is : " + math(2,4));
+// console.log("inside math module value is : " + math.add(2,4));
+
+// 
+// console.log("inside math module value is : " + math);
+console.log("inside math module value is : " , math);
+
+
 
 
