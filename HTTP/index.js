9436ed703e6d097ea36deb3793d1fb8601149528
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-09-07 12:54:27 +0530

    with url

--- HTTP/index.js
+++ HTTP/index.js
@@ -1,25 +1,30 @@
 const http = require('http');
-
 const fs = require('fs');
+const url = require('url');
 
 const myServer = http.createServer((req, res) => {
+    if(req.url === "/favicon.ico") return res.end();
     const log = `${Date.now()}:  ${req.url} New rwq recieved \n`;
+    const myUrl = url.parse(req.url , true);
+    console.log(myUrl);
+    
 
     fs.appendFile('log.txt', log, (err, data) => {
         
-        switch (req.url) {
+        switch (myUrl.pathname) {
             case "/" :
             res.end("homepage");
                 break;
             case "/about": 
-            res.end("hello i am saurabh tomer");
+                const username = myUrl.query.myname
+            res.end(`hello ${username}`);
                 break;
             default:
                 res.end("404 not found");
         }
     });
 
-    console.log(req);
+    // console.log(req);
 
     // console.log("new req rec.");
     // res.end('hello from server');
